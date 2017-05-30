0483e907ba5197adf102a1f1518123e52a3c6b2e
Jonathan Brazie <jon.brazie@gmail.com>
2017-05-29 20:38:09 -0700

    First commit

--- swiftapppp/ViewController.swift
+++ swiftapppp/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloworld: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func hellobutton(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        if tapCount >= 10 {
+            helloworld.text = "you tapped ten times"
+        }
+    }
+    
+    
+    @IBAction func coolbutton(_ sender: Any) {
+        helloworld.text = "Buttons are cool!"
+        
+        print("dontpushmetapped")
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
